287838dfc7ba675b4f1193d3ea3612fc28097078
jezing <jezing@foxmail.com>
2021-01-28 00:57:40 +0800

    about  babel first submit for learning

---
+++ babel and webpack/babel/dist/babel.js
@@ -0,0 +1,23 @@
+"use strict";
+
+require("./index.js");
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var name = "Alex";
+var age = 19;
+
+var add = function add(x, y) {
+    return x + y;
+};
+
+new Promise(function(resolve, reject) {
+    resolve("success");
+});
+Array.from([1, 2]);
+
+var Person = function Person() {
+    _classCallCheck(this, Person);
+};
+
+;
---
+++ babel and webpack/babel/src/babel.js
@@ -0,0 +1,9 @@
+let name = "Alex";
+const age = 19;
+const add = (x, y) => x + y;
+new Promise((resolve, reject) => {
+    resolve("success");
+});
+Array.from([1, 2]);
+class Person {};
+import './index.js';
