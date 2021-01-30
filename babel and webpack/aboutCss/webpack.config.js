300cb9e50b7cdc1e7d8d279474550eb6545b8c5c
jezing <jezing@foxmail.com>
2021-01-30 11:10:17 +0800

    handle the problem of the css in js ,webpack to pack the css to a single css file

--- babel and webpack/aboutCss/webpack.config.js
+++ babel and webpack/aboutCss/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: "development",
@@ -14,13 +15,17 @@ module.exports = {
         rules: [{
             test: /\.css$/,
             // loader: "css-loader" //单loader
-            use: ["style-loader", "css-loader"] //多loader时，注意顺序，从右往左运行
+            // use: ["style-loader", "css-loader"] //多loader时，注意顺序，从右往左运行
+            use: [MiniCssExtractPlugin.loader, "css-loader"]
         }]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./text.html",
             filename: "index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css"
         })
     ]
 
